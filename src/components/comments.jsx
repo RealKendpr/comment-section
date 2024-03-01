49bbc3a7965d5b7ec8348a2078c4a8ab331adad8
kendpr <real.kendpr@gmail.com>
2024-03-01 08:00:34 +0800

    Added an editing the comment functionality
    - Able to edit and update the comment
    - or cancel the editing

--- src/components/comments.jsx
+++ src/components/comments.jsx
@@ -13,47 +13,88 @@ export function Comments() {
   const [showConfirmation, setShowConfirmation] = useState(false);
   const [deleteComment, setDeleteComment] = useState(null);
 
+  const [editComment, setEditComment] = useState(null);
+  const [editedComment, setEditedComment] = useState("");
+
+  const handleEdit = (commentId, content) => {
+    setEditComment(commentId);
+    setEditedComment(content);
+  };
+  const handleCancel = () => {
+    setEditComment(null);
+    setEditedComment("");
+  };
+  const handleUpdate = () => {
+    const updatedComments = comments.map((comment) => {
+      if (comment.id === editComment) {
+        return {
+          ...comment,
+          content: editedComment,
+        };
+      }
+      return comment;
+    });
+    setComment(updatedComments);
+    setEditComment(null);
+    setEditedComment("");
+  };
+
   return (
     <>
       {comments.map((comment) => {
         const { id, user, createdAt, content, replies } = comment;
         return (
-          <article key={id}>
-            <div>
-              <img src={user.image.png} alt="" />
-              <a href="#">{user.username}</a>
-              <div>{createdAt}</div>
-            </div>
-            <p>{content}</p>
-            <div>
+          <>
+            <article key={id}>
+              <div>
+                <img src={user.image.png} alt="" />
+                <a href="#">{user.username}</a>
+                <div>{createdAt}</div>
+              </div>
               <Scores {...comment}></Scores>
-              {user.username === currentUser.username ? (
-                <div>
-                  <button>Edit</button>
-                  <DeleteBtn
-                    comment={comment}
-                    setDeleteComment={setDeleteComment}
-                    setShowConfirmation={setShowConfirmation}
-                  ></DeleteBtn>
-                </div>
-              ) : (
-                <button>Reply</button>
-              )}
-            </div>
-            <div className="replies">
-              {replies.length > 0 && <Replies></Replies>}
-            </div>
-            <ConfirmDelete
-              showConfirmation={
-                showConfirmation && comment.id === deleteComment
-              }
-              setDeleteComment={setDeleteComment}
-              deleteComment={deleteComment}
-              setShowConfirmation={setShowConfirmation}
-              comments={comments}
-              setComment={setComment}
-            ></ConfirmDelete>
-          </article>
+              <p>{content}</p>
+              <div>
+                {user.username === currentUser.username ? (
+                  <div>
+                    {editComment === id ? (
+                      <div>
+                        <textarea
+                          value={editedComment}
+                          onChange={(e) => setEditedComment(e.target.value)}
+                        ></textarea>
+                        <button onClick={handleUpdate}>Update</button>
+                        <button onClick={handleCancel}>Cancel</button>
+                      </div>
+                    ) : (
+                      <button onClick={() => handleEdit(id, content)}>
+                        Edit
+                      </button>
+                    )}
+                    <DeleteBtn
+                      comment={comment}
+                      setDeleteComment={setDeleteComment}
+                      setShowConfirmation={setShowConfirmation}
+                    ></DeleteBtn>
+                  </div>
+                ) : (
+                  <button>Reply</button>
+                )}
+              </div>
+              <div className="replies">
+                {replies.length > 0 && <Replies></Replies>}
+              </div>
+              <ConfirmDelete
+                showConfirmation={
+                  showConfirmation && comment.id === deleteComment
+                }
+                setDeleteComment={setDeleteComment}
+                deleteComment={deleteComment}
+                setShowConfirmation={setShowConfirmation}
+                comments={comments}
+                setComment={setComment}
+              ></ConfirmDelete>
+            </article>
+          </>
         );
       })}
       <div className="userInputs">
